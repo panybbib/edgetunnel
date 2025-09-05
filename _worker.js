c85a5acf234bbf653584b2cfd30f413f931b17bc
panybbib <pan.107@163.com>
2025-09-05 19:28:06 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1784,7 +1784,7 @@ async function 生成配置信息(userID, hostName, sub, UA, RproxyIP, _url, fak
             } else {
                 const response = await fetch(url, {
                     headers: {
-						'User-Agent': 'v2rayN' + atob('IENGLVdvcmtlcnMtZWRnZXR1bm5lbC9jbWxpdQ==')
+                        'User-Agent': 'Mozilla/5.0' + atob('IENGLVdvcmtlcnMtZWRnZXR1bm5lbC9jbWxpdQ==')
                     }
                 });
                 content = await response.text();
@@ -4568,4 +4568,3 @@ async function nginx() {
 	`
     return text;
 }
-
